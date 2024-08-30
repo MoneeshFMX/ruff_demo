7af6f3bbaefc0ad1054935ba8d20e44465a57056
Moneesh FMX <moneesh.kumar@fehrmann-materialsx.com>
2024-08-30 14:20:09 +0200

    Fixed linting and formatting

--- helper.py
+++ helper.py
@@ -9,4 +9,3 @@ def find_weighted_average(scores_array: np.array, weights_array: np.array):
 def add_arrays(scores_array: np.array, weights_array: np.array):
     """Compute the element-wise sum of two arrays."""
     return np.add(scores_array, weights_array)
-
